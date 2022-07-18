afaab2c304a6b1f5931fb3e4b029b292240de71e
poojaamar <pamar6401@gmail.com>
2022-07-18 17:16:29 +0530

    uc-3 snake ladder switch case

--- Day4assignment/Day4assignment/Program.cs
+++ Day4assignment/Day4assignment/Program.cs
@@ -1,6 +1,21 @@
-﻿Console.WriteLine("Welcome to Snake & Ladder Game Program.");
+﻿const int LADDER = 1;
+const int SNAKE = 2;
+Console.WriteLine("Welcome to Snake & game problem.");
 int position = 0;
 Random random = new Random();
+int option = random.Next(0, 3);
 int rolldie = random.Next(1, 7);
 Console.WriteLine("Player 1 position is " + position);
-Console.WriteLine("Roll Die value: " + rolldie);
+switch (option)
+{
+    case LADDER:
+        position += rolldie;
+        break;
+    case SNAKE:
+        position -= rolldie;
+        break;
+    default:
+        position += 0;
+        break;
+}
+Console.WriteLine("Player 1 position is " + position);
