3bb60729f4bcc3e5d58a8c142ddc86fb1d3c2e8f
poojaamar <pamar6401@gmail.com>
2022-07-18 17:04:10 +0530

    uc-2 snake & ladders rolldie problem

--- Day4assignment/Day4assignment/Program.cs
+++ Day4assignment/Day4assignment/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Welcome to Snake & Ladder Game Program.");
 int position = 0;
+Random random = new Random();
+int rolldie = random.Next(1, 7);
 Console.WriteLine("Player 1 position is " + position);
+Console.WriteLine("Roll Die value: " + rolldie);
